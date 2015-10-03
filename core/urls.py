13d522fc10cf75437badf53da5876e6dbced8dbd
msowder <msowder@wellesley.edu>
2015-10-03 19:53:37 -0400

    sorry maddie

--- core/urls.py
+++ core/urls.py
@@ -1,1 +1,8 @@
-__author__ = 'msowder'
+from django.conf.urls import url
+
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    url(r'tasks/(?P<task_id>[0-9]+)/$', views.get_tasks, name='view_task'),
+]
