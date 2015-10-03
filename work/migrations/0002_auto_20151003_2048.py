bf0a2f9d7772c6abaaab30209f4e5dd5cdb4782e
msowder <msowder@wellesley.edu>
2015-10-03 18:35:18 -0400

    git is weird

---
+++ work/migrations/0002_auto_20151003_2048.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('polls', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Tasks',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=200)),
+                ('due_date', models.DateTimeField()),
+                ('points', models.IntegerField(default=0)),
+                ('category', models.CharField(max_length=200)),
+                ('difficulty', models.CharField(max_length=200)),
+            ],
+        ),
+        migrations.RemoveField(
+            model_name='choice',
+            name='question',
+        ),
+        migrations.DeleteModel(
+            name='Choice',
+        ),
+        migrations.DeleteModel(
+            name='Question',
+        ),
+    ]
---
+++ work/migrations/0003_account_calendar_rewards.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('polls', '0002_auto_20151003_2048'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Account',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=200)),
+                ('photo', models.FilePathField(path=None)),
+                ('points', models.IntegerField(default=0)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Calendar',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=200)),
+                ('category', models.CharField(max_length=200)),
+                ('start_time', models.DateTimeField()),
+                ('end_time', models.DateTimeField()),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Rewards',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=200)),
+                ('category', models.CharField(max_length=200)),
+                ('points', models.IntegerField(default=0)),
+                ('priority', models.CharField(max_length=200)),
+                ('start_time', models.DateTimeField()),
+                ('end_time', models.DateTimeField()),
+            ],
+        ),
+    ]
